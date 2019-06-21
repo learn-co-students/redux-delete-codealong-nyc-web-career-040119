9f9ce5e2cfea5f59d3886046e5f6638ed2463289
Edgar Ong <emailedgar@gmail.com>
2019-06-21 11:08:52 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,8 +3,22 @@ export default function manageTodo(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
+      console.log('ADD_TODO', action.payload)
 
-      return { todos: state.todos.concat(action.payload.text) };
+      const todo = {
+        id: Math.random()*10000000000000000,
+        text: action.payload.text
+      }
+
+      return { todos: state.todos.concat(todo) };
+
+    case 'DELETE_TODO':
+      console.log('DELETE_TODO', state)
+      const stateCopy = [...state.todos]
+      const filterText = stateCopy.filter(todo => todo.id !== action.payload)
+      console.log(filterText)
+
+      return { todos: filterText }
 
     default:
       return state;
